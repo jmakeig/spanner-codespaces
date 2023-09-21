8fef31e5b7b4fba8c5344c6c5197ecc9504cc262
Justin Makeig <jmakeig@google.com>
2023-09-21 23:45:38 +0000

    Wires up Node and pg client

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Hello, Spanner!');
+// https://stackoverflow.com/questions/55377103/can-i-import-the-node-postgres-module-pg-or-is-it-commonjs-only#comment131338205_55378800
+import pg from 'pg'
+
+const { Pool } = pg
+const pool = new Pool();
+
+console.log(pool);
